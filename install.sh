d8b393aa538afca09db5c7487d80f2cfc8f2fb74
Jean Simas <jeansimas16@gmail.com>
2022-06-13 23:58:47 -0300

    Adding font and fixing bugs

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 cargo install exa bat
@@ -29,6 +27,7 @@ rm -rf ~/Downloads/google-chrome-stable_current_amd64.deb
 wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
 sudo tar -xvf ~/Downloads/go1.18.3.linux-amd64.tar.gz -C /usr/local
 rm -rf ~/Downloads/go1.18.3.linux-amd64.tar.gz
-
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 reboot
 
